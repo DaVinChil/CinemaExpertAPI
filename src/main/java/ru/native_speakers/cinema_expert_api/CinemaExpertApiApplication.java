7e4aa0e7db08adf759e557153bbc5e3eda655a75
DaVinCi <fox.winner9@gmail.com>
2023-08-02 18:51:49 +0300

    Initial commit

---
+++ src/main/java/ru/native_speakers/cinema_expert_api/CinemaExpertApiApplication.java
@@ -0,0 +1,13 @@
+package ru.native_speakers.cinema_expert_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaExpertApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaExpertApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/native_speakers/cinema_expert_api/CinemaExpertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.native_speakers.cinema_expert_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaExpertApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
