3209ea0e7eb102f6fad0e2fcfd2f81e2df24d14d
Sekunov Ilya <sekunovila@gmail.com>
2023-08-11 15:31:51 +0300

    Images controller, service, repository + images service unit test add

--- src/main/java/ru/native_speakers/cinema_expert_api/repository/ImagesRepository.java
+++ src/main/java/ru/native_speakers/cinema_expert_api/repository/ImagesRepository.java
@@ -1,9 +1,16 @@
 package ru.native_speakers.cinema_expert_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.native_speakers.cinema_expert_api.model.Image;
 
+import java.util.List;
+
 @Repository
 public interface ImagesRepository extends JpaRepository<Image, Integer> {
+
+    @Query(value = "select * from images limit :count", nativeQuery = true)
+    List<Image> findAll(@Param("count") int count);
 }
---
+++ src/main/java/ru/native_speakers/cinema_expert_api/service/ImagesServiceImplementation.java
@@ -0,0 +1,31 @@
+package ru.native_speakers.cinema_expert_api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.native_speakers.cinema_expert_api.exception.EntityNotFoundException;
+import ru.native_speakers.cinema_expert_api.model.Image;
+import ru.native_speakers.cinema_expert_api.repository.ImagesRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ImagesServiceImplementation implements ImagesService {
+
+    private final ImagesRepository imagesRepository;
+
+    @Override
+    public Image findImageByImageId(int imageId) throws EntityNotFoundException {
+        Optional<Image> imageOptional = imagesRepository.findById(imageId);
+        if (imageOptional.isEmpty()) {
+            throw new EntityNotFoundException("Image with this id not found");
+        }
+        return imageOptional.get();
+    }
+
+    @Override
+    public List<Image> findImages(int count) {
+        return imagesRepository.findAll(count);
+    }
+}
