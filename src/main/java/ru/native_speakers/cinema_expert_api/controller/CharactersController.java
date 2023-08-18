95875d9f7dd7311e3a48647d03502bf970b47933
Ilya Sekunov <sekunovila@gmail.com>
2023-08-18 19:03:34 +0300

    CharacterController validation add

--- src/main/java/ru/native_speakers/cinema_expert_api/controller/CharactersController.java
+++ src/main/java/ru/native_speakers/cinema_expert_api/controller/CharactersController.java
@@ -1,5 +1,7 @@
 package ru.native_speakers.cinema_expert_api.controller;
 
+import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,13 @@ import ru.native_speakers.cinema_expert_api.dto.HttpEntityResponse;
 import java.util.List;
 
 @RequestMapping("/characters")
+@Validated
 public interface CharactersController {
 
     @GetMapping("/by-id/{id}")
-    HttpEntityResponse<CharacterDTO> getCharacterById(@PathVariable("id") long characterId);
+    HttpEntityResponse<CharacterDTO> getCharacterById(@PathVariable("id")
+                                                      @Min(value = 1, message = "Character's id cannot be less than 1")
+                                                      long characterId);
 
     @GetMapping("/by-name/{name}")
     HttpEntityResponse<List<CharacterDTO>> getCharactersByName(@PathVariable("name") String characterName);
