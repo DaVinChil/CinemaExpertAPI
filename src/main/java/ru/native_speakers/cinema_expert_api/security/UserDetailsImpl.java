7f1b882bac8baed364c4a6af36a5fad7ad7b48c5
Ilya Sekunov <sekunovila@gmail.com>
2023-08-24 19:27:56 +0300

    UserDetailsImpl add

---
+++ src/main/java/ru/native_speakers/cinema_expert_api/security/UserDetailsImpl.java
@@ -0,0 +1,45 @@
+package ru.native_speakers.cinema_expert_api.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.native_speakers.cinema_expert_api.model.User;
+
+import java.util.Collection;
+
+public record UserDetailsImpl(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
