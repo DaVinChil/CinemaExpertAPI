f5525a612a9cceb854e0f5727e477eb7610e8038
Ilya Sekunov <sekunovila@gmail.com>
2023-08-24 19:25:49 +0300

    UserServiceImpl add new features, password encoder

--- src/main/java/ru/native_speakers/cinema_expert_api/service/UserServiceImpl.java
+++ src/main/java/ru/native_speakers/cinema_expert_api/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package ru.native_speakers.cinema_expert_api.service;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,11 +16,16 @@ import java.util.Date;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -36,13 +41,16 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         }
         user.setCreatedAt(new Date());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
+        user.setAccountNonExpired(true);
+        user.setAccountNonLocked(true);
+        user.setCredentialsNonExpired(true);
         userRepository.save(user);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByUsername(username);
-        System.out.println(username);
 
         if (user.isEmpty()) {
             throw new UsernameNotFoundException("User with this username not found");
