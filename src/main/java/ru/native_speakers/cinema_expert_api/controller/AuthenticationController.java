bae24f492651edb7916d0675541408e3c72f7bfb
Ilya Sekunov <sekunovila@gmail.com>
2023-08-24 19:47:33 +0300

    delete useless imports

--- src/main/java/ru/native_speakers/cinema_expert_api/controller/AuthenticationController.java
+++ src/main/java/ru/native_speakers/cinema_expert_api/controller/AuthenticationController.java
@@ -1,7 +1,6 @@
 package ru.native_speakers.cinema_expert_api.controller;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
