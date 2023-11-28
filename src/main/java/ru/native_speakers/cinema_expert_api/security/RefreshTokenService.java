3db2f26b8fc97bf7428499ad0370914dd31f8e10
Sekunov Ilya <sekunovila@gmail.com>
2023-11-28 13:28:48 +0300

    refresh token generation change

--- src/main/java/ru/native_speakers/cinema_expert_api/security/RefreshTokenService.java
+++ src/main/java/ru/native_speakers/cinema_expert_api/security/RefreshTokenService.java
@@ -3,12 +3,12 @@ package ru.native_speakers.cinema_expert_api.security;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import ru.native_speakers.cinema_expert_api.dto.Token;
 
 import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.UUID;
 
 @Component
 @Data
@@ -17,11 +17,10 @@ public class RefreshTokenService {
 
     @Value("${refresh_token_days_lifetime}")
     private long refreshTokenDaysLifetime;
-    private final PasswordEncoder passwordEncoder;
 
     public Token generateToken(String username) {
         return Token.builder()
-                .value(passwordEncoder.encode(username))
+                .value(UUID.fromString(username).toString())
                 .issuedAt(new Date())
                 .expiresAt(Date.from(ZonedDateTime.now().plusDays(refreshTokenDaysLifetime).toInstant()))
                 .build();
